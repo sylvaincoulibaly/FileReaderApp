3cd01a5be3206acc81ec62e73f4d8ff43ddf3457
Sylvain Coulibaly <sylvaincoulibaly@yahoo.com>
2022-01-17 13:52:43 +0100

    Prise en compte de l'internationalisation de l'application

--- message_box.py
+++ message_box.py
@@ -1,14 +1,17 @@
-from PyQt5.QtWidgets import QMessageBox, QComboBox
+from PyQt5 import QtCore
+from PyQt5.QtWidgets import QMessageBox, QComboBox, QApplication
+
+_translate = QtCore.QCoreApplication.translate
 
 
 def message_box_critical():
     msg = QMessageBox()
     msg.setIcon(QMessageBox.Critical)
     # msg.setIcon(QMessageBox.Warning)
-    msg.setText("       Warning")
+    msg.setText(_translate("MainWindow", "       Warning"))
     # msg.setInformativeText('Vérifiez que vous avez choisi un fichier .csv ')
-    msg.setInformativeText('Choose a CSV file')
-    msg.setWindowTitle("Error ")
+    msg.setInformativeText(_translate("MainWindow", "Choose a CSV file"))
+    msg.setWindowTitle(_translate("MainWindow", "Error "))
     # msg.setStandardButtons(QMessageBox.Ok | QMessageBox.Cancel)
     msg.buttonClicked.connect(msgbtn)
     msg.exec_()
@@ -20,8 +23,8 @@ def message_box_critical_index_error():
     msg.setIcon(QMessageBox.Warning)
     # msg.setText("        Attention")
     # msg.setInformativeText('Vérifiez les colonnes sélectionnées et le séparateur de colonnes')
-    msg.setInformativeText('Invalid column separator')
-    msg.setWindowTitle(" Warning")
+    msg.setInformativeText(_translate("MainWindow", 'Invalid column separator'))
+    msg.setWindowTitle(_translate("MainWindow", " Warning"))
     # msg.setStandardButtons(QMessageBox.Ok | QMessageBox.Cancel)
     msg.buttonClicked.connect(msgbtn)
     # self.btn_plot.setEnabled(False)
@@ -34,8 +37,8 @@ def message_box_critical_file_not_found_error():
     msg.setIcon(QMessageBox.Warning)
     # msg.setText(" Attention")
     # msg.setInformativeText('Vérifiez les colonnes sélectionnées et le séparateur de colonnes')
-    msg.setInformativeText('No file selected')
-    msg.setWindowTitle("Warning")
+    msg.setInformativeText(_translate("MainWindow", 'No file selected'))
+    msg.setWindowTitle(_translate("MainWindow", "Warning"))
     # msg.setStandardButtons(QMessageBox.Ok | QMessageBox.Cancel)
     msg.buttonClicked.connect(msgbtn)
     msg.exec_()
@@ -45,16 +48,18 @@ def message_box_critical_index_error_x_column(combobox, column_number):
     msg = QMessageBox()
     msg.setIcon(QMessageBox.Critical)
     # msg.setIcon(QMessageBox.Warning)
-    msg.setText("         Warning")
+    msg.setText(_translate("MainWindow", "         Warning"))
     # msg.setInformativeText(
     #     'La colonne x sélectionnée n\'existe pas avec le séparateur de colonnes \'{}\' choisi. \n Si vous '
     #     'avez choisi le bon séparateur alors sélectionnez une autre colonne x'.format(
     #         self.csv_separator_combo.currentText()))
-    msg.setInformativeText('The selected {}-column doesn\'t \nexist with the chosen column separator.\n'
-                           ' If \"{}\" is the correct column separator then select another {}-column'.format(
+    msg.setInformativeText(_translate("MainWindow", "The selected {}-column doesn\'t exist with the "
+                                                    "chosen column separator.\n"
+                                                    "If \"{}\" is the correct column separator "
+                                                    "then select another {}-column".format(
         column_number,
-        combobox.currentText(), column_number))
-    msg.setWindowTitle("Error")
+        combobox.currentText(), column_number)))
+    msg.setWindowTitle(_translate("MainWindow", "Error"))
     # msg.setStandardButtons(QMessageBox.Ok | QMessageBox.Cancel)
     msg.buttonClicked.connect(msgbtn)
     msg.exec_()
@@ -64,17 +69,18 @@ def message_box_critical_index_error_y_column(combobox):
     msg = QMessageBox()
     msg.setIcon(QMessageBox.Critical)
     # msg.setIcon(QMessageBox.Warning)
-    msg.setText("    Warning")
+    msg.setText(_translate("MainWindow", "    Warning"))
     # msg.setInformativeText('La colonne y sélectionnée n\'existe pas avec ce séparateur de colonnes')
     # msg.setInformativeText(
     #     'La colonne y sélectionnée n\'existe pas avec le séparateur de colonnes \'{}\' choisi. \n Si vous '
     #     'avez choisi le bon séparateur alors sélectionnez une autre colonne y'.format(
     #         self.csv_separator_combo.currentText()))
-    msg.setInformativeText('The selected y-column doesn\'t \nexist with the chosen column separator.\n'
-                           ' If \"{}\" is the correct column separator then select another y-column'.format(
-        combobox.currentText()))
+    msg.setInformativeText(_translate("MainWindow", "The selected y-column doesn\'t exist with the "
+                                                    "chosen column separator.\n") +
+                           _translate("MainWindow", "If ") + f"{combobox.currentText()} " +
+                           _translate("MainWindow", "is the correct column separator then select another y-column."))
 
-    msg.setWindowTitle("Error")
+    msg.setWindowTitle(_translate("MainWindow", "Error"))
     # msg.setStandardButtons(QMessageBox.Ok | QMessageBox.Cancel)
     msg.buttonClicked.connect(msgbtn)
     msg.exec_()
